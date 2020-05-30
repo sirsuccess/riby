ea283a8a3eb2bba2232a45f1f263e5bf660a8abd
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-05-30 11:18:52 +0100

    modify controllers for seperation of concern

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,36 +1,77 @@
+const {
+  getActors,
+  getActorStreak,
+  updateActor,
+  selectAllActor,
+} = require("../queries/index");
+const db = require("../config/db");
 
-var getAllActors = () => {
-	
+const getAllActors = (req, res) => {
+  try {
+    db.all(getActors, (err, rows) => {
+      const items = [];
+      rows.map((item) => {
+        const { actor_id, avatar_url, login } = item;
+        items.push({ avatar_url, id: actor_id, login });
+      });
+      if (err) {
+        throw new Error(err);
+      }
+      return res.status(200).json(items);
+    });
+  } catch (error) {
+    res.status(404).json({ error: err.message });
+  }
 };
 
-var updateActor = () => {
+const updateActorCtr = (req, res) => {
+  try {
+    const { id, login, avatar_url } = req.body;
 
+    db.all(selectAllActor, id, (err, rows) => {
+      if (err) {
+        throw new Error(err);
+        //res.status(404).json({ error: err.message });
+        return;
+      }
+      if (rows.length < 1) {
+        // throw new Error("item not found");
+        return res.status(404).json({
+          message: "item not found",
+        });
+      }
+    });
+
+    db.run(updateActor, [id, login, avatar_url, id], function (err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+      res.status(200).json({});
+    });
+  } catch (error) {}
 };
 
-var getStreak = () => {
-
+const getStreak = (req, res) => {
+  try {
+    db.all(getActorStreak, (err, rows) => {
+      const items = [];
+      rows.map((item) => {
+        const { actor_id, avatar_url, login } = item;
+        items.push({ avatar_url, id: actor_id, login });
+      });
+      if (err) {
+        throw new Error(err);
+      }
+      return res.status(200).json(items);
+    });
+  } catch (error) {
+    res.status(404).json({ error: err.message });
+  }
 };
-
 
 module.exports = {
-	updateActor: updateActor,
-	getAllActors: getAllActors,
-	getStreak: getStreak
+  updateActorCtr: updateActorCtr,
+  getAllActors: getAllActors,
+  getStreak: getStreak,
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- controllers/events.js
+++ controllers/events.js
@@ -1,28 +1,192 @@
 const db = require("../config/db");
+const {
+  deleteEvents,
+  deleteRepos,
+  deleteActors,
+  getAllEvents,
+  getAllActorEvents,
+  postActor,
+  postRepo,
+  postEvent,
+  eventSelectAllActor,
+  eventSelectAllRepo,
+} = require("../queries/index");
 
-var getAllEvents = () => {
-  const sql = "select * from events";
-  const params = [];
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      return { error: err.message };
-    }
-    return {
-      message: "success",
-      data: rows,
-    };
-  });
+const getAllEventsCtrl = (req, res) => {
+  try {
+    db.all(getAllEvents, (err, rows) => {
+      if (err) {
+        throw new Error(err.message);
+        //res.status(400).json({ error: err.message });
+        return;
+      }
+      let eventData = [];
+      rows.map((event) => {
+        //destructure event
+        const {
+          eventID,
+          type,
+          actor_id,
+          login,
+          avatar_url,
+          repo_id,
+          name,
+          url,
+          created_at,
+        } = event;
+
+        //push to eventData arr
+        eventData.push({
+          id: eventID,
+          type,
+          actor: {
+            id: actor_id,
+            login,
+            avatar_url,
+          },
+          repo: {
+            id: repo_id,
+            name,
+            url,
+          },
+          created_at,
+        });
+      });
+      return res.status(200).json(eventData);
+    });
+  } catch (error) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
-var addEvent = () => {};
+//add events, repos, and actors
+const addEvent = (req, res) => {
+  try {
+    const { id, type, actor, repo, created_at } = req.body;
 
-var getByActor = () => {};
+    var eventParams = [id, type, repo.id, actor.id, created_at];
+    var actorParams = [actor.id, actor.login, actor.avatar_url];
+    var repoParams = [repo.id, repo.name, repo.url];
 
-var eraseEvents = () => {};
+    db.all(eventSelectAllActor, actor.id, (err, rows) => {
+      //if not found
+      if (rows.length < 1) {
+        db.run(postActor, actorParams, function (err, result) {
+          if (err) {
+            throw new Error(err.message);
+          }
+        });
+      }
+    });
+
+    db.all(eventSelectAllRepo, repo.id, (err, rows) => {
+      //if not found
+      if (rows.length < 1) {
+        db.run(postRepo, repoParams, function (err, result) {
+          if (err) {
+            throw new Error(err.message);
+          }
+        });
+      }
+    });
+
+    db.run(postEvent, eventParams, function (err, result) {
+      if (err) {
+        throw new Error(err.message);
+      }
+      res.status(201).json({
+        message: "success",
+      });
+    });
+  } catch (error) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+//get events by actor id
+const getByActor = (req, res) => {
+  try {
+    const params = req.params.id;
+    db.all(getAllActorEvents, params, (err, rows) => {
+      if (err) {
+        throw new Error(err.message);
+        //res.status(400).json({ error: err.message });
+        return;
+      }
+      //if not found
+      if (rows.length < 1) {
+        res.status(404).json({
+          message: "item not found",
+        });
+      }
+
+      let eventData = [];
+      rows.map((event) => {
+        //destructure event
+        const {
+          eventID,
+          type,
+          actor_id,
+          login,
+          avatar_url,
+          repo_id,
+          name,
+          url,
+          created_at,
+        } = event;
+
+        //push to eventData arr
+        eventData.push({
+          id: eventID,
+          type,
+          actor: {
+            id: actor_id,
+            login,
+            avatar_url,
+          },
+          repo: {
+            id: repo_id,
+            name,
+            url,
+          },
+          created_at,
+        });
+      });
+      return res.status(200).json(eventData);
+    });
+  } catch (error) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+const eraseEvents = (req, res) => {
+  try {
+    db.run(deleteActors, function (err, result) {
+      if (err) {
+        throw new Error(err.message);
+      }
+    });
+    db.run(deleteEvents, function (err, result) {
+      if (err) {
+        throw new Error(err.message);
+      }
+    });
+    db.run(deleteRepos, function (err, result) {
+      if (err) {
+        throw new Error(err.message);
+      }
+    });
+    res.status(200).json({});
+  } catch (error) {
+    res.status(400).json({
+      message: "failed",
+    });
+  }
+};
 
 module.exports = {
-  getAllEvents: getAllEvents,
-  addEvent: addEvent,
-  getByActor: getByActor,
-  eraseEvents: eraseEvents,
+  getAllEventsCtrl,
+  addEvent,
+  getByActor,
+  eraseEvents,
 };
